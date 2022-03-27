fedfc1bf290a82a1fbcdd6fcfd8992ff86db05af
Alan Row <alekroga@gmail.com>
2022-03-27 17:28:33 +0500

    Fix error: use getDate instead of getDay

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,5 @@
 export function getDateLine(date, seperator) {
-  const dayLine = date.getDay().toString().padStart(2, "0");
+  const dayLine = date.getDate().toString().padStart(2, "0");
   const monthLine = (date.getMonth() + 1).toString().padStart(2, "0");
   const yearLine = date.getFullYear().toString();
   return [dayLine, monthLine, yearLine].join(seperator);
